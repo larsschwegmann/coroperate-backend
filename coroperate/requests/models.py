d8afe41b214fd62be2b214c779d9ed6cc4bd50a4
Leo Tappe <tappeleo@gmail.com>
2020-03-21 18:26:34 +0100

    add model skeletons

--- coroperate/requests/models.py
+++ coroperate/requests/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    A user profile for storing information about the user.
+    """
+    pass
+
+
+class Request(models.Model):
+    """
+    A request for a shopping service.
+    """
+    pass
+
+
+class Rating(models.Model):
+    """
+    A rating for a user by another user.
+    """
+    pass
+
+
+class ShoppingList(models.Model):
+    """
+    A list of shopping items.
+    """
+    pass
+
+
+class ShoppingItem(models.Model):
+    """
+    An item in a shopping list.
+    """
+    pass
