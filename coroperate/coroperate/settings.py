6f567305c2af9973f0487b272a3e35dbdb8a347a
Leo Tappe <tappeleo@gmail.com>
2020-03-21 17:59:01 +0100

    add rest framework and requests app to installed apps

--- coroperate/coroperate/settings.py
+++ coroperate/coroperate/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'requests.apps.RequestsConfig'
 ]
 
 MIDDLEWARE = [
