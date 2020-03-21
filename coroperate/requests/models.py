036e9e8ad3fe90ef36a0a24fa8167cf1eae4623b
Leo Tappe <tappeleo@gmail.com>
2020-03-22 00:32:59 +0100

    refactor models

--- coroperate/requests/models.py
+++ coroperate/requests/models.py
@@ -7,9 +7,9 @@ class Profile(models.Model):
     A user profile for storing information about the user.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=16)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    phone = models.CharField(max_length=16, default='default phone')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, default=0)
     radius = models.PositiveSmallIntegerField(default=3)
     verified = models.BooleanField(default=False)
 
@@ -18,14 +18,14 @@ class Request(models.Model):
     """
     A request for a shopping service.
     """
-    shopping_list = models.OneToOneField(shopping_list, on_delete=models.CASCADE)
-    address = models.CharField(max_length=32)
-    zip_code = models.CharField(max_length=10)
-    city = models.CharField(max_length=32)
-    county = models.CharField(max_length=32)
+    owner = models.ForeignKey(User, related_name='owned_requests')
+    max_price = models.DecimalField(max_digits=6, decimal_places=2, default=20)
+    address = models.CharField(max_length=32, default='default street')
+    zip_code = models.CharField(max_length=10, default='default zip code')
+    city = models.CharField(max_length=32, default='default city')
     tip = models.PositiveSmallIntegerField(default=0)
     date = models.DateTimeField(auto_now_add=True)
-    accepted = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    acceptor = models.ForeignKey(User, related_name='accepted_requests', null=True, on_delete=models.SET_NULL)
 
 
 class Rating(models.Model):
@@ -40,24 +40,17 @@ class Rating(models.Model):
         (5, 'fantastic')
     ]
     author = models.ForeignKey(User, related_name='authored_ratings', on_delete=models.CASCADE)
-    rated = models.ForeignKey(User, related_name='received_ratings', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_ratings', on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(choices=CHOICES)
     comment = models.TextField(max_length=128, blank=True, default='')
 
     class Meta:
-        unique_together = ['author', 'rated']
+        unique_together = ['author', 'receiver']
 
 
-class ShoppingList(models.Model):
+class Item(models.Model):
     """
-    A list of shopping items.
+    An item requested in a request.
     """
-    max_price = models.DecimalField(max_digits=6, decimal_places=2)
-
-
-class ShoppingItem(models.Model):
-    """
-    An item in a shopping list.
-    """
-    shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE)
+    request = models.ForeignKey(Request, related_name='items', on_delete=models.CASCADE)
     item = models.CharField(max_length=64)
