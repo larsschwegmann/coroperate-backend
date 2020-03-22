94c8dd85f4dcd858b1ae6348ddf20a0c4e508559
Johannes Müller <johannes.ru.mueller@gmail.com>
2020-03-22 14:19:01 +0100

    Testing setup + User Creation Test Case

--- coroperate/coroperate/settings.py
+++ coroperate/coroperate/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication'
-    ]
+    ],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 MIDDLEWARE = [
--- coroperate/requests/tests.py
+++ coroperate/requests/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase, force_authenticate
+
+from request.models import Profile
 
 # Create your tests here.
+class UserCreationTestCase(APITestCase):
+    def setUp(self):
+        self.userName = "Frank"
+        self.password = 'secret_password'
+
+    def test_1(self):
+        response = self.client.post('/users/', {'username': 'Frank', 'password': 'secret_Frank'})
+        print(response.data)
+
