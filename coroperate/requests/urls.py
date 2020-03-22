1665b578ebdc56b50b77bec3b1775f55f50d0299
Leo Tappe <tappeleo@gmail.com>
2020-03-22 02:40:03 +0100

    working /requests endpoint

---
+++ coroperate/requests/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from requests import views
+
+
+urlpatterns = [
+    path('', views.RequestList.as_view())
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- coroperate/requests/views.py
+++ coroperate/requests/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from requests.models import Request
+from requests.serializers import RequestSerializer
 
 # Create your views here.
+class RequestList(generics.ListCreateAPIView):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
